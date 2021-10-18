6cc11669604abc09e942a81a3503db2eb6fcd3d4
Andrey606 <andreykuluev96@gmail.com>
2021-10-18 03:32:20 +0300

    sdf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.allowed_request_origins = ['https://andrews-message-me-5.herokuapp.com', 'http://andrews-message-me-5.herokuapp.com']
-
 end
