b0f9782d3e6ae942670f6ce08f4e0ddcbf873fc4
Andrey606 <andreykuluev96@gmail.com>
2021-10-18 03:06:44 +0300

    finished

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomController < ApplicationController
+  before_action :requere_user
 
   def index
+    @message = Message.new
+    @messages = Message.custom_display
   end
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Andrew1", password: "123")
+User.create(username: "Andrew2", password: "123")
+User.create(username: "Andrew3", password: "123")
+User.create(username: "Andrew4", password: "123")
+User.create(username: "Andrew5", password: "123")
